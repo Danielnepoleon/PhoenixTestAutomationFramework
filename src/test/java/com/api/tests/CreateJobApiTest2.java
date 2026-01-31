1a40231d02f3b485ff92bb1c8ebb3f70f492bb3a
Danielnepoleon <danielnepoleon6994@gmail.com>
2026-01-31 15:39:58 +0530

    Added the changes related to DB data providers

--- src/test/java/com/api/tests/CreateJobApiTest2.java
+++ src/test/java/com/api/tests/CreateJobApiTest2.java
@@ -1,35 +1,20 @@
 package com.api.tests;
 
-import static com.api.utils.DateTimeUtil.getTimeWithDaysAgo;
+import static com.api.utils.SpecUtils.requestSpecWithAuthBody;
+import static com.api.utils.SpecUtils.responseSpec_OK;
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.startsWith;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.api.constants.Models;
-import com.api.constants.OEM;
-import com.api.constants.Platform;
-import com.api.constants.Problem;
-import com.api.constants.Products;
 import com.api.constants.Role;
-import com.api.constants.ServiceLocation;
-import com.api.constants.WARRANTY_STATUS;
 import com.api.request.model.CreateJobPayload;
-import com.api.request.model.Customer;
-import com.api.request.model.CustomerAddress;
-import com.api.request.model.CustomerProduct;
-import com.api.request.model.Problems;
 import com.api.utils.FakerDataGenerator;
-
-import static com.api.utils.SpecUtils.*;
-
-import static io.restassured.module.jsv.JsonSchemaValidator.*;
 
 public class CreateJobApiTest2 {
 
@@ -40,7 +25,8 @@ public class CreateJobApiTest2 {
 		createJobPayload = FakerDataGenerator.generateFakeCreateJobData();
 	}
 
-	@Test(description = "Verify if the create job api is able to create Inwarranty job", groups = { "api", "regression", "smoke" })
+	@Test(description = "Verify if the create job api is able to create Inwarranty job", groups = { "api", "regression",
+			"smoke" })
 	public void createJobApiTest() throws IOException {
 		given().spec(requestSpecWithAuthBody(Role.FD, createJobPayload)).when().post("/job/create").then()
 				.spec(responseSpec_OK()).body(matchesJsonSchemaInClasspath("response-schema/CreateJobApiSchema.json"))
--- src/test/java/com/api/utils/AuthTokenGenerator.java
+++ src/test/java/com/api/utils/AuthTokenGenerator.java
@@ -1,9 +1,10 @@
 package com.api.utils;
 
-import static io.restassured.RestAssured.*;
-
-import java.io.IOException;
-import static com.api.constants.Role.*;
+import static com.api.constants.Role.ENG;
+import static com.api.constants.Role.FD;
+import static com.api.constants.Role.QC;
+import static com.api.constants.Role.SUP;
+import static io.restassured.RestAssured.given;
 
 import com.api.constants.Role;
 import com.api.request.model.Usercredentials;
@@ -11,33 +12,30 @@ import com.api.request.model.Usercredentials;
 import io.restassured.http.ContentType;
 
 public class AuthTokenGenerator {
-	
+
 	private AuthTokenGenerator() {
-		
+
 	}
-	
+
 	public static String getToken(Role role) {
 		Usercredentials credentials = null;
 		if (role == FD) {
-			credentials = new Usercredentials("iamfd" , "password");
+			credentials = new Usercredentials("iamfd", "password");
 		} else if (role == SUP) {
-			credentials = new Usercredentials("iamsup" , "password");
+			credentials = new Usercredentials("iamsup", "password");
 		} else if (role == ENG) {
-			credentials = new Usercredentials("iameng" , "password");
+			credentials = new Usercredentials("iameng", "password");
 		} else if (role == QC) {
-			credentials = new Usercredentials("iamqc" , "password");
+			credentials = new Usercredentials("iamqc", "password");
 		}
 		String token = null;
-		try {
-			token = given().baseUri(ConfigManager.getProperty("BASE_URI")).contentType(ContentType.JSON)
-			.body(credentials).when().post("login").then()
-			.log().ifValidationFails().statusCode(200).extract().jsonPath().getString("data.token");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
+		token = given().baseUri(ConfigManager.getProperty("BASE_URI")).contentType(ContentType.JSON).body(credentials)
+				.when().post("login").then().log().ifValidationFails().statusCode(200).extract().jsonPath()
+				.getString("data.token");
+
 		return token;
-		
+
 	}
 
 }
